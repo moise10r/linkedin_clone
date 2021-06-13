f901f47c361e09e9bcc3dacaf47b15d45b96b40d
moise10r <moiserushanika2006@gmail.com>
2021-06-13 22:10:21 +0200

    Initial commit.

---
+++ src/components/navBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+	return <nav className="main-navbar-container">hello</nav>;
+};
+
+export default NavBar;
---
+++ src/components/pages/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import NavBar from "../navBar";
+
+class Home extends Component {
+	state = {};
+	render() {
+		return (
+			<div className="main-navbar-container">
+				<NavBar />
+			</div>
+		);
+	}
+}
+
+export default Home;
